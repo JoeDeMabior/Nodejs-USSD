00a46673c39eaaf7efee648e5a7551d2de89501b
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-06-28 10:38:45 +0300

    Clean up the code

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const indexRouter = require('./index');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`running on localhost:${PORT}`));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
-
-module.exports = app;
